cdb6c6228cc0ea28b0661084570d49b98a69f986
Andrew Kreps <andrewkreps@lululemon.com>
2017-06-07 18:43:22 -0700

    Actual race results

--- winner.rb
+++ winner.rb
@@ -1,8 +1,9 @@
 require 'sequel'
 require 'pg'
 
+DB = Sequel.connect('postgres://root:b01762cfbe4ab3de@portlandracing.c6aanljryr7p.us-west-2.rds.amazonaws.com:5432/winnerwinner') # Uses the postgres adapter
 
-yao = {}
+racer_laps_by_plate_number = {}
 run_array = []
 laps = {1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => [], 10 => []}
 
@@ -17,34 +18,35 @@ end
 # Open up the results file (plate numbers, one per line)
 
 File.open(ARGV[0]).each_with_index do |line, index|
-  num = line.chomp
-  unless yao[num]
-    yao[num] = 0
+  plate_number = line.chomp
+  unless racer_laps_by_plate_number[plate_number]
+    racer_laps_by_plate_number[plate_number] = 0
   end
-  yao[num] += 1
+  racer_laps_by_plate_number[plate_number] += 1
   per_lap_count += 1
-  laps[yao[num]].push num
+  laps[racer_laps_by_plate_number[plate_number]].push plate_number
 
-  if yao[num] > max_occurrence
+  if racer_laps_by_plate_number[plate_number] > max_occurrence
     max_occurrence += 1
     run_array.push "Lap #{max_occurrence} (#{per_lap_count} riders)"
     per_lap_count = 0
   end
-  run_array.push "#{num} occurrence #{yao[num]}"
+  run_array.push "#{plate_number} occurrence #{racer_laps_by_plate_number[plate_number]}"
 end
 
-puts yao.inspect
+puts racer_laps_by_plate_number.inspect
 puts run_array.join("\n")
 
-laps.each do |k, lap|
-  puts "Lap #{k}, #{lap.count} riders"
+printed = {}
+
+laps.keys.sort.reverse.each do |key|
   # puts lap.join "\n"
-  lap.each do |rider_num|
-    print rider_num
+  puts "Lap #{key}, #{laps[key].count} riders"
+  laps[key].each do |rider_num|
     query = "SELECT first_name, last_name, plate_number, racing_age FROM racers where plate_number = #{rider_num} and category = #{ARGV[1]}"
     DB.fetch(query) do |row|
-      print ",#{row[:first_name]} #{row[:last_name][0]},#{row[:racing_age]}"
+      puts "#{rider_num},#{row[:first_name]} #{row[:last_name][0]},#{row[:racing_age]}" unless printed[rider_num]
     end
-    puts
+    printed[rider_num] = true
   end
 end
